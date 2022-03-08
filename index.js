05d6763c670424a84cbe8006f869bacdabf25465
Kamlesh <kamlesh_fw11_179@masai.school>
2022-03-09 00:23:54 +0530

    fixed the hasing and added env variables

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const {Client} = require("pg")
+const bcrypt = require('bcryptjs');
 const Joi = require("joi")
 const date = new Date()
 const {ReasonPhrases,StatusCodes,getReasonPhrase,getStatusCode,} = require('http-status-codes');
+const { STATUS_CODES } = require('http');
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'fastApi',
-    password: '123456789',
-    port: 5432,
+    user: process.env.POSTGRES_USERNAME,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
 })
 try{
 client.connect()
@@ -41,8 +44,11 @@ app.post("/users",async(req,res)=>{
     try{
     const value = await schema.validateAsync({email:req.body.email,username:req.body.username,password:req.body.password});
     const insert_query = "INSERT INTO users(email,password,created_at) VALUES($1,$2,$3) RETURNING *"
-    const response = await client.query(insert_query,[value.email,value.password,date])
-    return response
+    let hashed_password = bcrypt.hashSync(value.password, 8)
+    const response = await client.query(insert_query,[value.email,hashed_password,date])
+    if(response){
+        res.status(StatusCodes.CREATED).send({message:getReasonPhrase(StatusCodes.CREATED)})
+    }
     }catch(err){
         res.status(StatusCodes.NOT_ACCEPTABLE).send({error:getReasonPhrase(StatusCodes.NOT_ACCEPTABLE),info:err})
     }
