da610cdfc825a2674fd1b68c49a81a20761815cb
Kamlesh <kamlesh_fw11_179@masai.school>
2022-03-09 23:25:38 +0530

    some minor changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bcrypt = require('bcryptjs');
 const Joi = require("joi")
 const date = new Date()
 const {ReasonPhrases,StatusCodes,getReasonPhrase,getStatusCode,} = require('http-status-codes');
-const { STATUS_CODES } = require('http');
 const client = new Client({
     user: process.env.POSTGRES_USERNAME,
     host: process.env.POSTGRES_HOST,
@@ -14,6 +13,21 @@ const client = new Client({
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
 })
+const createTable = `CREATE TABLE users (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL
+)`;
+
+const res = `CREATE TABLE restaurents(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    locations VARCHAR(50) UNIQUE NOT NULL,
+    price_range int NOT NULL check(price_range>=1 and price_range<=5)
+);`;
+// INSERT into restaurents(name,locations,price_range) values('pizzastop','miami',3)
 try{
 client.connect()
 }catch(err){
@@ -40,12 +54,33 @@ app.get("/users",async(req,res)=>{
     }
 })
 
+// get all restaurents
+
+app.get("/api/v1",async(req,res)=>{
+    try{
+    const response  = await client.query("SELECT * FROM restaurents")
+    console.log(response)
+    res.status(200).send(response.rows)
+    }catch(err){
+        console.log(err)
+        res.send("Something broke")
+    }
+})
+app.get("/api/v1/:id",async(req,res)=>{
+    try{
+        const response = await client.query(`SELECT * FROM restaurents WHERE id = ${req.params.id}`)
+        res.status(StatusCodes.OK).json({"restaurent":response.rows})
+    }catch(err){
+        console.log(err)
+        res.send(StatusCodes.INTERNAL_SERVER_ERROR).json({error:getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)})
+    }
+})
 app.post("/users",async(req,res)=>{
     try{
     const value = await schema.validateAsync({email:req.body.email,username:req.body.username,password:req.body.password});
-    const insert_query = "INSERT INTO users(email,password,created_at) VALUES($1,$2,$3) RETURNING *"
+    const insert_query = "INSERT INTO users(username,email,password,created_at) VALUES($1,$2,$3,$4) RETURNING *"
     let hashed_password = bcrypt.hashSync(value.password, 8)
-    const response = await client.query(insert_query,[value.email,hashed_password,date])
+    const response = await client.query(insert_query,[value.username,value.email,hashed_password,date])
     if(response){
         res.status(StatusCodes.CREATED).send({message:getReasonPhrase(StatusCodes.CREATED)})
     }
