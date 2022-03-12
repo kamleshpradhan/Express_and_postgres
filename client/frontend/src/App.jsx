52502226192e6a8f4889c6d3925912127bd86ad5
Kamlesh <kamlesh_fw11_179@masai.school>
2022-03-12 19:11:37 +0530

    client side added vite-react used

---
+++ client/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// import { Button } from 'react-bootstrap';
+export default function App(){
+  return(
+    <>
+    <div style={{textAlign:"center"}}>
+      <h1 style={{"fontWeight":"100","margin":"auto","marginTop":"50px"}}>Hello World</h1>
+      </div>
+    </>
+  )
+}
